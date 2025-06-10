ba5d0de286551d9a987a7feaaffcb68b2f294b29
Kai Bertelsen <kai@exposoft.no>
2025-06-10 10:56:16 +0200

    ny

---
+++ apploader.js
@@ -0,0 +1,31 @@
+function loadScript(url) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = () => resolve();
+        script.onerror = () => reject(`Failed to load script: ${url}`);
+        document.head.appendChild(script);
+    });
+}
+
+// Liste over CDN-URL-er som skal lastes inn
+const cdnScripts = [
+    "https://kaibertelsen.github.io/serviceloop/startup.js"
+];
+
+// Laste inn alle skriptene sekvensielt
+cdnScripts.reduce((promise, script) => {
+    return promise.then(() => loadScript(script));
+}, Promise.resolve()).then(() => {
+    console.log("All scripts loaded");
+    MemberStack.onReady.then(function(member) {
+        if (member.loggedIn){
+         
+        }else{
+     
+        }
+    });
+
+}).catch(error => {
+    console.error(error);
+});
--- test.js
+++
@@ -1,5 +0,0 @@
-function test(){
-//dette er en test
-
-
-}
